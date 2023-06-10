6b994c9b6acc32112b5aecc2cfe7303b5b2e66cd
Amani Alessa <124394159+Amanixxx@users.noreply.github.com>
2023-06-10 14:21:51 +0300

    update -1

--- World/resources/views/members.blade.php
+++
@@ -1,7 +0,0 @@
-
-@extends('layouts.app')
-@section('content')
-<div class="container" dir="rtl">
-    <h1>الاعضاء والمشاركين   </h1>
-</div>
-    @endsection
--- World/resources/views/news.blade.php
+++
@@ -1,9 +0,0 @@
-
-
-
-@extends('layouts.app')
-@section('content')
-<div class="container" dir="rtl">
-    <h1> الاخبار والاحداث   </h1>
-</div>
-@endsection
